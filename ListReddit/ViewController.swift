243aa6564dd1a588bdaa88a0b1f9fcd9e9368109
Marvin Do <marvin.do@everykey.com>
2018-07-24 19:10:59 -0700

    first commit

---
+++ ListReddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListReddit
+//
+//  Created by Marvin Do on 7/24/18.
+//  Copyright © 2018 Marvin Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
